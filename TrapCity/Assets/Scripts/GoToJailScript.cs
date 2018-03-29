b1b615e797657760857db9c45639139fd5f1e61e
Cullen <cullenb0916@hotmail.com>
2018-03-29 18:20:12 -0400

    Add files via upload

---
+++ TrapCity/Assets/Scripts/GoToJailScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToJailScript : MonoBehaviour, TileScript
+{
+    private GameObject player;
+
+    // sending player to jail
+    public override void Activate(GameObject player)
+    {
+        // dont know how to do yet
+    }
+}
---
+++ TrapCity/Assets/Scripts/JailScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JailScript : MonoBehaviour, TileScript
+{
+    private int escapeRollNum;
+    // need to set bail price
+    private int bailPrice;
+
+    // get bail price
+    public int GetBailPrice()
+    {
+        return bailPrice;
+    }
+
+    // Increments how many turns the player has been in jail. Game manager will need to determine 
+    // if the player needs to force the player to pay the bail after three rolls
+    public int IncrementEscapeRollNum()
+    {
+        escapeRollNum++;
+        return escapeRollNum;
+    }
+}
