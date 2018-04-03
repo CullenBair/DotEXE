9be0105333c94d877a2e7627f7b08202f7611601
joepeaden <joe.peaden@gmail.com>
2018-04-03 10:22:03 -0400

    Updated Player, Die, and GameManager in order to set up game control and state machines

--- TrapCity/Assets/Scripts/DieScript.cs
+++ TrapCity/Assets/Scripts/DieScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class DieScript : MonoBehaviour
 {
-    public GameManager gm;
+    // The following block is singleton pattern, basically
+    // it should give an access point to it from all gameobjects by calling
+    // DieScript.instance();
+    // see http://gameprogrammingpatterns.com/singleton.html for details
+    private static DieScript instance_;
+    private DieScript() {}
+    public static DieScript instance()
+    {
+        if(instance_ == null)
+            instance_ = GameObject.FindObjectOfType<DieScript>();
+        return instance;
+    }
+    ////////////////////////////////
 
+    public GameManager gm;
+    public static const STANDARD_SIDES = 6;
     int prevDieRoll = 0;
     int die1;
     int die2;
-    
+    // player needs to know as well, so this is set to
+    // true if this particular roll was a double
+    bool lastDouble = false;
+
+    void Start()
+    {
+        // reference to the game manager
+        gm = GameManager.instance();
+    }
+
     //rolls two dice, upadtes previous die roll and possibly calls incDouble in gamemanager
     public int RollDie(int numSides)
     {
+        lastDouble = false;
         die1 = Random.Range(1, numSides+1);
         die2 = Random.Range(1, numSides+1);
 
@@ -24,6 +48,26 @@ public class DieScript : MonoBehaviour
         return prevDieRoll = die1 + die2;
     }
 
+    // method overload to handle standart sided die rolls
+    public int RollDie()
+    {
+        lastDouble = false;
+        die1 = Random.Range(1, STANDARD_SIDES+1);
+        die2 = Random.Range(1, STANDARD_SIDES+1);
+
+        if (isDouble())
+        {
+            gm.incDouble();
+        }
+
+        return prevDieRoll = die1 + die2;
+    }
+
+    public bool wasLastDouble()
+    {
+        return lastDouble;
+    }
+
     public int GetPrevDieRoll()
     {
         return prevDieRoll;
@@ -31,6 +75,11 @@ public class DieScript : MonoBehaviour
 
     public bool isDouble()
     {
-        return (die1 == die2);
+        if(die1 == die2)
+        {
+            lastIsDouble = true;
+            return true;
+        }
+        return false;
     }
 }
--- TrapCity/Assets/Scripts/PlayerScript.cs
+++ TrapCity/Assets/Scripts/PlayerScript.cs
@@ -10,15 +10,53 @@ public class PlayerScript : MonoBehaviour
     private int timeInJail;
     private int playerLocationIndex;
     private bool myTurn;
+    private GameManagerScript gm;
+    private DieScript die;
+
+    // enum for possible states
+    private enum State { Active, Rolling, Waiting };
 
     private void Start()
     {
+        // setting initial state
+        State state = State.Waiting;
+        // reference to die
+        die = DieScript.instance();
+        // reference to the game manager
+        gm = GameManager.instance();
         //override this from game manager
         playerIndex = -1;
         timeInJail = 0;
         playerLocationIndex = 0;
     }
-    
+
+    // Rolls die, if double then state == active,
+    // else state == waiting and return control
+    // to gamemanger
+    public void Roll()
+    {
+        int roll = die.RollDie();
+        if(die.wasLastDouble())
+            // NOTE: THIS SHOULD BE SET TO ACTIVE. HAVE NOT FIGURED OUT HOW TO DEAL WITH
+            // THE DOUBLE ROLL STATE. FOR NOW WILL JUST IMMEDIATELY ROLL
+            state = State.Rolling;
+        else
+            state = State.Active;
+    }
+
+    // Starts player turn
+    public void StartTurn()
+    {
+        state = State.Active;
+    }
+
+    // Ends player turn
+    private void EndTurn();
+    {
+        state = State.Waiting;
+        gm.NextTurn();
+    }
+
     // Request trade from another player, return true if accepted, false otherwise
     public bool RequestTrade(int playerReciever)
     {
@@ -26,9 +64,9 @@ public class PlayerScript : MonoBehaviour
         //return person.RecieveTrade()
 
         //trade button disabled when not turn by gamemanager
-        
+
         return false;
-        
+
     }
 
     public bool RecieveTrade(int playerRequester)
@@ -47,7 +85,7 @@ public class PlayerScript : MonoBehaviour
         GameManager gm = GetComponent<GameManager>();
         Board board = GetComponent<Board>();
         // Looks for any money to be gained (penality if can't)
-        int propLocationIndex = gm.playerList[gm.lastRolledPlayerIndex].playerLocationIndex; 
+        int propLocationIndex = gm.playerList[gm.lastRolledPlayerIndex].playerLocationIndex;
         if (board.owner[propLocationIndex] == this.playerIndex)
         {
             //gets money
