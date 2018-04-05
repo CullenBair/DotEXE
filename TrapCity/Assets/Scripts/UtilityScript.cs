219b0199e8709a302432c3e9c59ca066e98cbf54
Joe Peaden <joe.peaden@gmail.com>
2018-04-05 11:41:02 -0400

    Update UtilityScript.cs

--- TrapCity/Assets/Scripts/UtilityScript.cs
+++ TrapCity/Assets/Scripts/UtilityScript.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UtilityScript : MonoBehaviour, TileScript, IBuyTile
+public class UtilityScript : TileScript, IBuyTile
 {
 	private int[] rent = {4,10};
 	private int rentIndex;
 	private int mortgagePrice;
 	private bool isMortgaged;
 	private DieScript die;
+
+	public override void Activate()
+	{
+		print("Activate not yet implemented");
+	}
 
 	//Assign the die object to "die" for referencing.
 	public void Start()
