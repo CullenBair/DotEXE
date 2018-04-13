8fca33dd29f31c0965dc1d8dd04ea9b72d4005a6
Cullen <cullenb0916@hotmail.com>
2018-04-13 13:21:53 -0400

    Add files via upload

---
+++ TrapCity/Assets/NewPlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class NewPlayerScript : MonoBehaviour {
+
+    public string playerName;
+    private GameManagerScript gm;
+
+    // Use this for initialization
+    void Start () {
+        gm = GameManagerScript.instance();
+	}
+
+    void newUser()
+    {
+        playerName = GetComponent<InputField>().text;
+        gm.socket.Emit("new user", JsonConvert.SerializeObject(playerName)); // JsonConvert.SerializeObject(gm.FirstPlayer)
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+    }
+
+
+    
+}
