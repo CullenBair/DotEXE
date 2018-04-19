ce7c174212a9ab4995731f433e62b138e2639418
Cullen <cullenb0916@hotmail.com>
2018-04-18 23:02:10 -0400

    Add stuff
    Player button increase, displays current player, buy upgrades, sell upgrades, mortgage

---
+++ TrapCity/Assets/Scripts/Tiles/GoToJailScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToJailScript : TileScript
+{
+    public string descript;
+    private GameObject player;
+    private GameManagerScript gm;
+
+
+    private void Start()
+    {
+        gm = GameManagerScript.instance();
+    }
+
+    /*             TILESCRIPT INHERITANCE                */
+
+    // sending player to jail
+    public override void Activate()
+    {
+        gm.GetCurrentPlayer().GetComponent<PlayerScript>().SetLocIndex(10);
+        gm.GetCurrentPlayer().GetComponent<PlayerScript>().transform.position = gm.GetTile(10).transform.position;
+        gm.GetCurrentPlayer().GetComponent<PlayerScript>().SetStateToJail();
+        gm.NextTurn();
+    }
+
+    // Displaying tile info
+    public override string TileInfo()
+    {
+        return descript;
+    }
+}
